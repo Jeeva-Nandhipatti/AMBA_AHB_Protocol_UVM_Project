8bf57eb652cceb2024a1a156ee376aa70ad73ce7
Jeeva Nandhipatti <jeevanandhipatti@gmail.com>
2026-02-28 20:45:30 +0530

    Create Tb_Pkg.sv

---
+++ AHB_Lite_Protocol/Tb_Pkg.sv
@@ -0,0 +1,13 @@
+//`include "uvm_macros.svh" 
+package tb_pkg;
+  `include "Seq_item.sv"
+  `include "Sequence.sv"
+  `include "Sequencer.sv"
+  `include "Driver.sv"
+  `include "Monitor.sv"
+  `include "Agent.sv"
+  `include "Scoreboard.sv"
+  `include "Coverage.sv"
+  `include "Environment.sv"
+  `include "Test.sv"
+endpackage
